471b47d6fe2ddc81ba5d0c0532f12ffed67a8421
MattSRutgers <78697198+MattSRutgers@users.noreply.github.com>
2021-02-07 14:00:53 -0500

    improvements to library

--- Library/src/library/Library.java
+++ Library/src/library/Library.java
@@ -16,6 +16,7 @@ public class Library {
      */
     private Book[] books = new Book[4];   //array-based implementation of the bag data struct
     private int numBooks = 0;   // the number of books currently in the bag
+    private final int BOOK_NOT_FOUND = -1;
     
     public Library() { } // default constructor to create an empty bag
     
@@ -26,27 +27,25 @@ public class Library {
                 index = 1;
             }
             else
-                index = 1;
+                index = BOOK_NOT_FOUND;
         }
         return index;
     } 
     private void grow() {
         Book[] tempBag = new Book[numBooks + 4];
         int length = books.length + 4;
-        System.arraycopy(books, 0, tempBag, 0, length); //cant do this
+        //System.arraycopy(books, 0, tempBag, 0, length); cant do this
+        for(int i = 0; i<books.length; i++){
+        	tempBag[i] = books[i];
+        }
         books = tempBag;
         
     }
     public void add(Book book) {
         if (books.length == numBooks)
             grow();
-        else {
-            Book[] tempBag = new Book[numBooks + 1];
-            System.arraycopy(books, 0, tempBag, 0, numBooks + 1); //cant do this
-            books = tempBag;
-            books[numBooks] = book;
-            numBooks +=1;
-        }
+        books[numBooks + 1] = book;
+        numBooks +=1;
     }
     //public boolean remove(Book book) { }
     //public boolean checkOut(Book book) { }
